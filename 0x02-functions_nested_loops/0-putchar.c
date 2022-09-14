555ecb780dfbc03be6898123da8a2b64af4ebb2f
root <nablintech@gmail.com>
2022-09-13 23:36:46 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,27 @@
-#include "main.h" 
-/** 
-* print_alphabet - function to print abc
-* 
-*Return: 0  
-void print_alphabet(void)
-{
-                 char c;  
+#include "main.h"
 
-                   for (c = 'a'; c <= 'z'; c++)
-                   {
-                                _putchar('\n');
-                         }
+/**
+ * main - print putchar 
+ * Return: always 0
+ *
+/
+
+int main(void)
+{
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+
+_putchar('t');
+
+_putchar('c');
+
+_putchar('h');
+
+_putchar('a');
+
+_putchar('r');
+
+_putchar('\n');
+
+return (0);
